257ad9c96125b84f437da5eb29c1509906ad4a2b
fintechlmt <pranavgaba11@gmail.com>
2020-08-09 19:00:25 +0530

    added timer after signup

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,10 +13,23 @@ export class RegisterComponent implements OnInit {
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+  timeLeft: number = 5;
+  interval;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 5;
+      }
+    }, 1000)
   }
 
   onSubmit() {
@@ -24,6 +38,10 @@ export class RegisterComponent implements OnInit {
         console.log(data);
         this.isSuccessful = true;
         this.isSignUpFailed = false;
+        this.startTimer()
+        setTimeout(() => {
+          this.router.navigate(['signin']);
+        }, 5000);
       },
       err => {
         this.errorMessage = err.error.message;
